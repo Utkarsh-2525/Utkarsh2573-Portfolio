d66fbf48032ea29a5fc2be5b7c9abed4efd0cae2
Utkarsh-2525 <2101641530158@psit.ac.in>
2024-07-07 00:51:58 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["a Student", "a DSA Learner", "an aspiring Software Developer"],
+    typeSpeed: 50,
+    backSpeed: 35,
+    backDelay: 1000,
+    loop: true
+});
